13985db86679fb5e40492bb2af37adfc649e03ac
Elber Funez <elberfunez@gmail.com>
2022-11-16 20:03:35 -0500

    user controller working

---
+++ HelpDeskApi/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelpDeskApi;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ICollection<Ticket> Tickets { get; } = new List<Ticket>();
+
+    public virtual ICollection<UserFavorite> UserFavorites { get; } = new List<UserFavorite>();
+}
--- HelpDeskApi/Program.cs
+++ HelpDeskApi/Program.cs
@@ -1,13 +1,35 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using HelpDeskApi;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+// add cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+// Add Db Context
+builder.Services.AddDbContext<HelpDeskAppDbContext>(options => options.UseSqlServer());
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,7 +38,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+// Activate Cors ( REMEMBER!!)
+app.UseCors();
 
 app.UseAuthorization();
 
